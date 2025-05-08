0e5e8004c5677aa8474203d001771853f2f98bd4
trax48 <raxgaming480@gmail.com>
2025-05-08 07:56:30 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -17,7 +17,7 @@ async def start(client, message):
         await client.send_message(LOG_CHANNEL, script.LOG_TEXT_P.format(message.from_user.id, message.from_user.mention))
     rm = InlineKeyboardMarkup(
         [[
-            InlineKeyboardButton("✨ Update Channel", url="https://t.me/vj_botz")
+            InlineKeyboardButton("✨ Update Channel", url="https://t.me/Ace_Files")
         ]] 
     )
     await client.send_message(
@@ -51,20 +51,20 @@ async def stream_start(client, message):
         download = await get_shortlink(f"{URL}{str(log_msg.id)}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}")
         
     await log_msg.reply_text(
-        text=f"•• ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n•• ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n•• ᖴᎥᒪᗴ Nᗩᗰᗴ : {fileName}",
+        text=f"‣ ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ꜰᴏʀ ɪᴅ #{user_id} \n‣ ᴜꜱᴇʀɴᴀᴍᴇ : {username} \n\n‣ Fɪʟᴇ ɴᴀᴍᴇ : {fileName}",
         quote=True,
         disable_web_page_preview=True,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🚀 Fast Download 🚀", url=download),  # we download Link
-                                            InlineKeyboardButton('🖥️ Watch online 🖥️', url=stream)]])  # web stream Link
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("🔺 Fast Download 🔺", url=download),  # we download Link
+                                            InlineKeyboardButton('🔻 Watch online 🔻', url=stream)]])  # web stream Link
     )
     rm=InlineKeyboardMarkup(
         [
             [
-                InlineKeyboardButton("sᴛʀᴇᴀᴍ 🖥", url=stream),
-                InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ 📥", url=download)
+                InlineKeyboardButton("sᴛʀᴇᴀᴍ 🔺", url=stream),
+                InlineKeyboardButton("ᴅᴏᴡɴʟᴏᴀᴅ 🔻", url=download)
             ]
         ] 
     )
-    msg_text = """<i><u>𝗬𝗼𝘂𝗿 𝗟𝗶𝗻𝗸 𝗚𝗲𝗻𝗲𝗿𝗮𝘁𝗲𝗱 !</u></i>\n\n<b>📂 Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>📦 Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b>📥 Dᴏᴡɴʟᴏᴀᴅ :</b> <i>{}</i>\n\n<b> 🖥ᴡᴀᴛᴄʜ  :</b> <i>{}</i>\n\n<b>🚸 Nᴏᴛᴇ : ʟɪɴᴋ ᴡᴏɴ'ᴛ ᴇxᴘɪʀᴇ ᴛɪʟʟ ɪ ᴅᴇʟᴇᴛᴇ</b>"""
+    msg_text = """<i><u>‣ ʏᴏᴜʀ ʟɪɴᴋ ɢᴇɴᴇʀᴀᴛᴇᴅ ! ⚡... !</u></i>\n\n<b>‣ Fɪʟᴇ ɴᴀᴍᴇ :</b> <i>{}</i>\n\n<b>‣ Fɪʟᴇ ꜱɪᴢᴇ :</b> <i>{}</i>\n\n<b> Dᴏᴡɴʟᴏᴀᴅ🔻:</b> <i>{}</i>\n\n<b> sᴛʀᴇᴀᴍ🔺 :</b> <i>{}</i>\n\n<b>‣ ❤️ Powered By : @Ace_Files✨😎</b>"""
 
     await message.reply_text(text=msg_text.format(get_name(log_msg), humanbytes(get_media_file_size(message)), download, stream), quote=True, disable_web_page_preview=True, reply_markup=rm)
